9b8b2fd0dde97025248ffbbc3adc72240afc5d4b
Odin Thif <mjd.thif@icloud.com>
2025-12-03 12:43:20 +0100

    Refactor server initialization to use StartServer function and handle unhandled rejections

--- booking_service/src/index.ts
+++ booking_service/src/index.ts
@@ -1,4 +1,22 @@
-import { createApp } from "./app";
-createApp().listen(3000, "0.0.0.0", () => {
-  console.log(`Server started on http://localhost:3000.`);
-});
+import { createApp } from "./expressApp";
+
+const PORT = process.env.PORT || 3000;
+
+export const StartServer = async () => {
+  createApp().listen(PORT, () => {
+    console.log(`Server started on ${PORT}`);
+  });
+
+  process.on("unhandledRejection", (reason, promise) => {
+    console.error("Unhandled Rejection at:", promise, "reason:", reason);
+    process.exit(1);
+  });
+};
+
+StartServer()
+  .then(() => {
+    console.log("Application started successfully.");
+  })
+  .catch((error) => {
+    console.error("Failed to start application:", error);
+  });
