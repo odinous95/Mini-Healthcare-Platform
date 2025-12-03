060fe76e9a64f5f1f560d03e0f31915e2e30c43e
Odin Thif <mjd.thif@icloud.com>
2025-12-03 17:24:17 +0100

    feat: add MockBookingRepository implementation for IBookingCore

---
+++ booking_service/src/repository/mock_boking.repository.ts
@@ -0,0 +1,14 @@
+import { IBookingCore } from "../interfaces/IBookingCore";
+import { Appointment } from "../models/appointment.model";
+
+export class MockBookingRepository implements IBookingCore {
+  async create(data: Appointment): Promise<Appointment> {
+    // Implementation here
+    throw new Error("Method not implemented.");
+  }
+
+  async findAll(): Promise<Appointment | null> {
+    // Implementation here
+    throw new Error("Method not implemented.");
+  }
+}
