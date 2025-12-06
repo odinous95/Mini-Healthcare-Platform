ce9a407763366e3adb521493a43e0902aa10139c
Odin Thif <mjd.thif@icloud.com>
2025-12-07 00:51:06 +0100

    feat: implement mock repository methods for appointment creation and retrieval

--- booking_service/src/repositories/mock_boking.repository.ts
+++ booking_service/src/repositories/mock_boking.repository.ts
@@ -2,13 +2,40 @@ import { IAppointmentCore } from "../interfaces/IAppointmentCore";
 import { Appointment } from "../models/appointment.model";
 
 export class MockBookingRepository implements IAppointmentCore {
+  private appointments: Appointment[];
+
+  constructor() {
+    this.appointments = [
+      {
+        patientName: "John Doe",
+        doctorName: "Dr. Smith",
+        appointmentDate: new Date("2023-10-01T10:00:00Z"),
+        reason: "Regular Checkup",
+        id: 1,
+      },
+      {
+        patientName: "fsdf Doe",
+        doctorName: "Dr. fasd",
+        appointmentDate: new Date("2023-10-01T10:00:00Z"),
+        reason: "fasdf Checkup",
+        id: 2,
+      },
+    ];
+  }
+
   async createAppointment(data: Appointment): Promise<Appointment> {
-    return Promise.resolve(data);
+    const newAppointment = new Appointment(
+      data.patientName,
+      data.doctorName,
+      data.appointmentDate,
+      data.reason,
+      this.appointments.length + 1
+    );
+    this.appointments.push(newAppointment);
+    return Promise.resolve(newAppointment);
   }
 
   async getAppointments(): Promise<Appointment[] | null> {
-    // return an empty array for the mock repository
-    const appointments: Appointment[] = [];
-    return Promise.resolve(appointments);
+    return Promise.resolve(this.appointments);
   }
 }
