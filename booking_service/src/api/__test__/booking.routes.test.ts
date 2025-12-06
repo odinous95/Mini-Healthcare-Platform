83aa03f09f9a676f9e145deb7b03df9cdfc97047
Odin Thif <mjd.thif@icloud.com>
2025-12-06 19:34:59 +0100

    test: update appointment date in mock request and add validation error handling tests

--- booking_service/src/api/__test__/booking.routes.test.ts
+++ booking_service/src/api/__test__/booking.routes.test.ts
@@ -12,7 +12,7 @@ const mockAppointmentRequset = () => {
   return {
     patientName: randFirstName(),
     doctorName: randFirstName(),
-    appointmentDate: new Date("2024-06-15T09:00:00Z"),
+    appointmentDate: new Date("2024-12-01T10:00:00Z"),
     reason: "Consultation",
   };
 };
@@ -30,10 +30,35 @@ describe("Booking Routes", () => {
         .post("/appointment")
         .send(mockReqPayload)
         .set("Accept", "application/json");
-      // Adjust for date serialization
       res.body.appointmentDate = new Date(res.body.appointmentDate);
       expect(res.status).toBe(201);
       expect(res.body).toEqual(appointment);
+    });
+
+    test("should return a validation error ", async () => {
+      const mockReqPayload = mockAppointmentRequset();
+      const res = await request(app)
+        .post("/appointment")
+        .send({ ...mockReqPayload, patientName: "" })
+        .set("Accept", "application/json");
+      expect(res.status).toBe(400);
+      expect(res.body.errors).toEqual("patientName should not be empty");
+    });
+
+    test("should response with internal error", async () => {
+      const mockReqPayload = mockAppointmentRequset();
+      jest
+        .spyOn(bookingUsecase, "createAppointment")
+        .mockImplementationOnce(() =>
+          Promise.reject(new Error("Internal Server Error"))
+        );
+
+      const res = await request(app)
+        .post("/appointment")
+        .send(mockReqPayload)
+        .set("Accept", "application/json");
+      expect(res.status).toBe(500);
+      expect(res.body).toEqual({ error: "Internal Server Error" });
     });
   });
 });
