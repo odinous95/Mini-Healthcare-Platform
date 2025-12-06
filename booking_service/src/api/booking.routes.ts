93430e48f632adc037590d69bf5644cc509e5bc1
Odin Thif <mjd.thif@icloud.com>
2025-12-06 19:35:04 +0100

    feat: enhance appointment creation with input validation and error handling

--- booking_service/src/api/booking.routes.ts
+++ booking_service/src/api/booking.routes.ts
@@ -2,6 +2,8 @@ import express, { NextFunction } from "express";
 import { Request, Response } from "express";
 import { BookingRepository } from "../repository";
 import { BookingUsecase } from "../usecases";
+import { CreateAppointmentRequest } from "../DTO";
+import { RequestValidator } from "./validations/requestValidator";
 
 const bookingRouter = express.Router();
 export const bookingUsecase = new BookingUsecase(new BookingRepository());
@@ -17,8 +19,19 @@ bookingRouter.get(
 bookingRouter.post(
   "/appointment",
   async (req: Request, res: Response, next: NextFunction) => {
-    const data = await bookingUsecase.createAppointment(req.body);
-    return res.status(201).json(data);
+    try {
+      const { errors, input } = await RequestValidator(
+        CreateAppointmentRequest,
+        req.body
+      );
+      if (errors) {
+        return res.status(400).json({ errors });
+      }
+      const data = await bookingUsecase.createAppointment(input);
+      return res.status(201).json(data);
+    } catch (error) {
+      return res.status(500).json({ error: (error as Error).message });
+    }
   }
 );
 
