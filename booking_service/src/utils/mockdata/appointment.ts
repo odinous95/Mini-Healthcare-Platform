a5219d7435e6ac5c17a989103ff850b290bb866e
Odin Thif <mjd.thif@icloud.com>
2025-12-07 18:49:10 +0100

    feat: update AppointmentFactory to generate random id for appointments

--- booking_service/src/utils/mockdata/appointment.ts
+++ booking_service/src/utils/mockdata/appointment.ts
@@ -3,7 +3,7 @@ import { Appointment } from "../../models/appointment.model";
 import { randFirstName, randNumber } from "@ngneat/falso";
 
 export const AppointmentFactory = new Factory<Appointment>()
-  .attr("id", () => randNumber({ min: 1, max: 1000 }))
+  .attr("id", +1)
   .attr("patientName", () => randFirstName())
   .attr("doctorName", () => randFirstName())
   .attr("appointmentDate", () => new Date("2024-06-15T09:00:00Z"))
