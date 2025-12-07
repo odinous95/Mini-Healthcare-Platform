175f5d90b58bdaa754bfbc2c7761cd0dc848c6d7
Odin Thif <mjd.thif@icloud.com>
2025-12-07 22:29:21 +0100

    add database configuration - Added @types/pg and drizzle-kit to devDependencies in package.json - Added drizzle-orm and pg to dependencies in package.json - Introduced DATABASE_URL in app.config.ts for database connection configuration

---
+++ booking_service/drizzle.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "drizzle-kit";
+import config from "./src/configs/app.config";
+
+export default defineConfig({
+  out: "./src/external.infrastructure/pg.db/migrations",
+  schema: "./src/external.infrastructure/pg.db/schema",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: config.DATABASE_URL!,
+  },
+});
--- booking_service/src/configs/app.config.ts
+++ booking_service/src/configs/app.config.ts
@@ -4,12 +4,14 @@ interface Config {
   port: number;
   nodeEnv: string;
   USE_MOCK: boolean;
+  DATABASE_URL: string;
 }
 
 const config: Config = {
   port: Number(process.env.PORT) || 3000,
   nodeEnv: process.env.NODE_ENV || "development",
   USE_MOCK: process.env.USE_MOCK === "true",
+  DATABASE_URL: process.env.DATABASE_URL || "",
 };
 
 export default config;
