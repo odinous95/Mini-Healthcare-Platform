8ba48025a328c73899e4394a3b590712aec14f15
Odin Thif <mjd.thif@icloud.com>
2025-12-07 14:35:32 +0100

    feat: add InversifyJS dependencies and reflect-metadata; create application constants for dependency injection

--- booking_service/src/index.ts
+++ booking_service/src/index.ts
@@ -1,3 +1,4 @@
+import "reflect-metadata";
 import { createApp } from "./expressApp";
 
 const PORT = process.env.PORT || 3000;
---
+++ booking_service/src/utils/applicationConstants.ts
@@ -0,0 +1,6 @@
+export const INTERFACE_TYPES = {
+  MockBookingRepository: Symbol.for("MockBookingRepository"),
+  BookingRepository: Symbol.for("BookingRepository"),
+  AppointmentUsecase: Symbol.for("AppointmentUsecase"),
+  AppointmentController: Symbol.for("AppointmentController"),
+};
