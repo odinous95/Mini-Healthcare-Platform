e4b517f83dac54f371899b49c3fae3fe9d2fc0ec
Odin Thif <mjd.thif@icloud.com>
2025-12-08 00:18:45 +0100

    fix: correct output and schema paths in drizzle configuration

--- booking_service/drizzle.config.ts
+++ booking_service/drizzle.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "drizzle-kit";
 import config from "./src/configs/app.config";
 
 export default defineConfig({
-  out: "./src/external.infrastructure/pg-db/migrations",
-  schema: "./src/external.infrastructure/pg-db/schema",
+  out: "./src/external.infrastructure/db-pg/migrations",
+  schema: "./src/external.infrastructure/db-pg/schema",
   dialect: "postgresql",
   dbCredentials: {
     url: config.DATABASE_URL!,
