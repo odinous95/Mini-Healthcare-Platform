32a56c2c9f57b0eebbc1682688cda82e33d97985
Odin Thif <mjd.thif@icloud.com>
2025-12-07 18:48:29 +0100

    feat: update booking routes tests to use AppointmentUsecase from DI container; remove unused mock function

--- booking_service/src/api/__test__/booking.routes.test.ts
+++ booking_service/src/api/__test__/booking.routes.test.ts
@@ -1,34 +1,44 @@
 import request from "supertest";
 import express from "express";
-import bookingRouter, { appointmentService } from "../booking.routes";
-import { randFirstName } from "@ngneat/falso";
+import bookingRouter from "../booking.routes";
 import { AppointmentFactory } from "../../utils/mockdata/appointment";
+import { DIcontainer } from "../../configs/inversify.config";
+import { INTERFACE_TYPES } from "../../utils";
+import { AppointmentUsecase } from "../../service";
 
 const app = express();
 app.use(express.json());
 app.use("/", bookingRouter);
 
-const mockAppointmentRequset = () => {
-  return {
-    patientName: randFirstName(),
-    doctorName: randFirstName(),
-    appointmentDate: new Date("2024-12-01T10:00:00Z"),
-    reason: "Consultation",
-  };
-};
+// Mocking the AppointmentUsecase from the DI container
+// const appointmentUsecase = DIcontainer.get<AppointmentUsecase>(
+//   INTERFACE_TYPES.AppointmentUsecase
+// );
 
-describe("Booking Routes", () => {
+describe("Booking Routes test | integration tests", () => {
+  // Define appointmentUsecase here to be used in tests
+  let appointmentUsecase: AppointmentUsecase;
+
+  beforeEach(() => {
+    // Setup  before each test
+    appointmentUsecase = DIcontainer.get<AppointmentUsecase>(
+      INTERFACE_TYPES.BookingRepository
+    );
+  });
+
+  afterEach(() => {
+    appointmentUsecase = {} as AppointmentUsecase;
+  });
+
   describe("POST /appointment", () => {
     test("should create an appointment", async () => {
-      const mockReqPayload = mockAppointmentRequset();
       const appointment = AppointmentFactory.build();
       jest
-        .spyOn(appointmentService, "createAppointment")
+        .spyOn(appointmentUsecase, "createAppointment")
         .mockImplementationOnce(() => Promise.resolve(appointment));
-
       const res = await request(app)
         .post("/appointment")
-        .send(mockReqPayload)
+        .send(appointment)
         .set("Accept", "application/json");
       res.body.appointmentDate = new Date(res.body.appointmentDate);
       expect(res.status).toBe(201);
@@ -36,29 +46,13 @@ describe("Booking Routes", () => {
     });
 
     test("should return a validation error ", async () => {
-      const mockReqPayload = mockAppointmentRequset();
+      const appointment = AppointmentFactory.build();
       const res = await request(app)
         .post("/appointment")
-        .send({ ...mockReqPayload, patientName: "" })
+        .send({ ...appointment, patientName: "" })
         .set("Accept", "application/json");
       expect(res.status).toBe(400);
       expect(res.body.errors).toEqual("patientName should not be empty");
-    });
-
-    test("should response with internal error", async () => {
-      const mockReqPayload = mockAppointmentRequset();
-      jest
-        .spyOn(appointmentService, "createAppointment")
-        .mockImplementationOnce(() =>
-          Promise.reject(new Error("Internal Server Error"))
-        );
-
-      const res = await request(app)
-        .post("/appointment")
-        .send(mockReqPayload)
-        .set("Accept", "application/json");
-      expect(res.status).toBe(500);
-      expect(res.body).toEqual({ error: "Internal Server Error" });
     });
   });
 });
