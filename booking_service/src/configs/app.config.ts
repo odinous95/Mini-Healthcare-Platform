73b45d39ec3fc09f8f2f6753e82bfd9cdb5fb2c6
Odin Thif <mjd.thif@icloud.com>
2025-12-10 00:19:11 +0100

    fix: update USE_MOCK type to string and adjust assignment in app.config

--- booking_service/src/configs/app.config.ts
+++ booking_service/src/configs/app.config.ts
@@ -3,14 +3,14 @@ import "dotenv/config";
 interface Config {
   port: number;
   nodeEnv: string;
-  USE_MOCK: boolean;
+  USE_MOCK: string;
   DATABASE_URL: string;
 }
 
 const config: Config = {
   port: Number(process.env.PORT) || 3000,
   nodeEnv: process.env.NODE_ENV || "development",
-  USE_MOCK: process.env.USE_MOCK === "true",
+  USE_MOCK: process.env.USE_MOCK!,
   DATABASE_URL: process.env.DATABASE_URL || "",
 };
 
