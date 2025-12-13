9a221b2985756a40540561bb1ccc2c15cd980e18
Odin Thif <mjd.thif@icloud.com>
2025-12-14 00:23:15 +0100

    feat: implement Kafka admin and producer connection methods in KafkaBroker

--- booking_service/src/external.infrastructure/kafka.broker/kafkaBroker.ts
+++ booking_service/src/external.infrastructure/kafka.broker/kafkaBroker.ts
@@ -1,12 +1,52 @@
+import { Admin, Consumer, Kafka, Producer } from "kafkajs";
 import { IMessageBroker } from "../../interfaces";
 
 export class KafkaBroker implements IMessageBroker {
-  // producer
-  connectProducer<T>(): Promise<T> {
-    throw new Error("Method not implemented.");
+  private admin?: Admin;
+  private producer?: Producer;
+  private consumer?: Consumer;
+
+  constructor(private kafka: Kafka) {}
+
+  // ---------- ADMIN ----------
+  async connectAdmin(): Promise<Admin> {
+    if (!this.admin) {
+      this.admin = this.kafka.admin();
+      await this.admin.connect();
+      console.log("Kafka Admin connected");
+    }
+    return this.admin;
   }
-  disconnectProducer<T>(): Promise<T> {
-    throw new Error("Method not implemented.");
+
+  async disconnectAdmin(): Promise<void> {
+    if (this.admin) {
+      await this.admin.disconnect();
+      this.admin = undefined;
+      console.log("Kafka Admin disconnected");
+    }
+  }
+
+  async createTopics(
+    topics: { topic: string; numPartitions: number }[]
+  ): Promise<boolean> {
+    if (!this.admin) {
+      throw new Error("Admin is not connected");
+    }
+    console.log("Creating topics:", topics);
+    return this.admin.createTopics({ topics });
+  }
+
+  // producer
+  async connectProducer<T>(): Promise<T> {
+    if (!this.producer) {
+      this.producer = this.kafka.producer();
+      await this.producer.connect();
+    }
+    return this.producer as unknown as Promise<T>;
+  }
+  async disconnectProducer<T>(): Promise<void> {
+    await this.producer?.disconnect();
+    this.producer = undefined;
   }
   async publish(data: unknown): Promise<boolean> {
     // Implementation for publishing a message to the specified queue
@@ -15,7 +55,7 @@ export class KafkaBroker implements IMessageBroker {
     return true;
   }
 
-  // consumer-=-==-=-=-=-=-=-=-=-==-=-=-=-=--=-==--=-=-=-=-=--=-==--=-=-
+  // consumer-=-==-=-=-=-=-=
 
   connectConsumer<T>(): Promise<T> {
     throw new Error("Method not implemented.");
