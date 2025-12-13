f36c0d4f4743cb98d8f7e2368e9e0bda3b2f90b1
Odin Thif <mjd.thif@icloud.com>
2025-12-14 00:23:40 +0100

    fix: update TOPIC_TYPE to reflect correct event naming in types.ts

--- booking_service/src/external.infrastructure/kafka.broker/types.ts
+++ booking_service/src/external.infrastructure/kafka.broker/types.ts
@@ -1,9 +1,9 @@
+export type TOPIC_TYPE = "BookingEvent";
+
 export enum BookingEvent {
   book_appointment = "book_appointment",
   cancel_appointment = "cancel_appointment",
 }
-
-export type TOPIC_TYPE = "BookingEvents";
 
 export interface IMessagePayload {
   headers?: Record<string, any>;
