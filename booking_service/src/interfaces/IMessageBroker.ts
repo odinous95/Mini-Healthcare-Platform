bfba053a75275ea8b12204aee09546ca130475ac
Odin Thif <mjd.thif@icloud.com>
2025-12-14 00:22:29 +0100

    refactor: update IMessageBroker interface for improved admin connection methods

--- booking_service/src/interfaces/IMessageBroker.ts
+++ booking_service/src/interfaces/IMessageBroker.ts
@@ -1,8 +1,15 @@
+import { Admin } from "kafkajs";
 export interface IMessageBroker {
+  // --- Admin ---
+  connectAdmin(): Promise<Admin>;
+  disconnectAdmin(): Promise<void>;
+  createTopics(
+    topics: { topic: string; numPartitions: number }[]
+  ): Promise<boolean>;
   //producer
   connectProducer: <T>() => Promise<T>;
-  disconnectProducer: <T>() => Promise<T>;
-  publish(data: unknown): Promise<boolean>;
+  disconnectProducer: <T>() => Promise<void>;
+  publish: (data: unknown) => Promise<boolean>;
 
   //consumer
   connectConsumer: <T>() => Promise<T>;
