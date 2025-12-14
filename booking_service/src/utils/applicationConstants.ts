64218ddcfbf50a82b564b123ce8b597aecd9f720
Odin Thif <mjd.thif@icloud.com>
2025-12-15 00:22:22 +0100

    fix: add MessageBroker to INTERFACE_TYPES for dependency injection

--- booking_service/src/utils/applicationConstants.ts
+++ booking_service/src/utils/applicationConstants.ts
@@ -1,6 +1,7 @@
 export const INTERFACE_TYPES = {
   MockBookingRepository: Symbol.for("MockBookingRepository"),
   BookingRepository: Symbol.for("BookingRepository"),
+  MessageBroker: Symbol.for("MessageBroker"),
   AppointmentUsecase: Symbol.for("AppointmentUsecase"),
   AppointmentController: Symbol.for("AppointmentController"),
 };
