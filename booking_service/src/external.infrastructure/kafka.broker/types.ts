ff63362b3c43c453e5beeec7cc58a32507e1ee4f
Odin Thif <mjd.thif@icloud.com>
2025-12-15 00:21:00 +0100

    fix: update TOPIC_TYPE and BookingEvents for consistency in types.ts

--- booking_service/src/external.infrastructure/kafka.broker/types.ts
+++ booking_service/src/external.infrastructure/kafka.broker/types.ts
@@ -1,12 +1,13 @@
-export type TOPIC_TYPE = "BookingEvent";
+export type TOPIC_TYPE = "appointments";
 
-export enum BookingEvent {
+export enum BookingEvents {
   book_appointment = "book_appointment",
   cancel_appointment = "cancel_appointment",
 }
 
 export interface IMessagePayload {
   headers?: Record<string, any>;
-  event: BookingEvent;
-  data: Record<string, any>;
+  event: BookingEvents;
+  topic: TOPIC_TYPE;
+  messages: Record<string, any>;
 }
